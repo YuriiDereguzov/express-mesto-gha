6477c93682596c6eef1d5bc2cd7ad0449e1fb319
Yurii Dereguzov <DereguzovYE@yandex.ru>
2023-05-15 15:44:50 +0500

    feat: добавить логирование запросов и ошибок

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // const path = require('path');
+// const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const routes = require('./routes');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
@@ -11,11 +12,13 @@ const app = express();
 
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
-
 // app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(requestLogger); // подключаем логгер запросов
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+app.use(errorLogger); // подключаем логгер ошибок
 app.use(errors());
 
 app.use((err, req, res, next) => {
